d94c89363b3a48d43fae6b08e0d8edc3e4cb950e
lincey jérôme <lj_jerome@hotmail.com>
2021-05-19 00:32:14 -0400

    remove cv

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,11 @@
 let clipboard = new ClipboardJS('.copy-email', {
-    text: function () {
-        return 'lj_jerome@hotmail.com';
-    }
+  text: function () {
+    return 'lj_jerome@hotmail.com';
+  },
 });
 clipboard.on('success', function (e) {
-    console.log(e);
+  console.log(e);
 });
 clipboard.on('error', function (e) {
-    console.log(e);
+  console.log(e);
 });
-
